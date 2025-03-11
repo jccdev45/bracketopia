1dc82b3c8683705cdda9be39f911a18fc99c1aa4
jccdev45 <jccdev45@gmail.com>
2025-03-11 12:46:02 -0400

    chore: 🔧 Comment out local Supabase URL in client files

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "./generated.types";
 
+// const supabaseUrl = 'http://127.0.0.1:54321'
+
 const SUPABASE_URL = "https://kidlbinekhuoqcqyupbn.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZGxiaW5la2h1b3FjcXl1cGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NjcwNDQsImV4cCI6MjA1NzA0MzA0NH0._gkD2i0tSXRgYRd2L-pGifyTCXrZesZ6hTiIg_A-uz0";
--- src/integrations/supabase/server.ts
+++ src/integrations/supabase/server.ts
@@ -2,10 +2,13 @@ import type { Database } from "@/integrations/supabase/generated.types";
 import { createServerClient } from "@supabase/ssr";
 import { parseCookies, setCookie } from "@tanstack/react-start/server";
 
+// const supabaseUrl = 'http://127.0.0.1:54321'
+
 export function createClient() {
   return createServerClient<Database>(
     // biome-ignore lint/style/noNonNullAssertion: <biome annoying>
     process.env.SUPABASE_URL!,
+    // supabaseUrl,
     // biome-ignore lint/style/noNonNullAssertion: <biome annoying>
     process.env.SUPABASE_ANON_KEY!,
     {
