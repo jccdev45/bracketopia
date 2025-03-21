112e5eb5ccde25bc865c4f0fd3abbed597299ac5
jccdev45 <jccdev45@gmail.com>
2025-03-21 18:30:16 -0400

    ♻️ refactor: Add type assertion for env vars

--- src/integrations/supabase/server.ts
+++ src/integrations/supabase/server.ts
@@ -2,33 +2,27 @@ import type { Database } from "@/integrations/supabase/generated.types";
 import { createServerClient } from "@supabase/ssr";
 import { parseCookies, setCookie } from "@tanstack/react-start/server";
 
-const SUPABASE_URL = process.env.LOCAL_SUPABASE_URL;
-const ANON_KEY = process.env.LOCAL_SUPABASE_ANON_KEY;
-// const ROLE_KEY = process.env.LOCAL_SUPABASE_ROLE_KEY;
+const SUPABASE_URL = process.env.LOCAL_SUPABASE_URL as string;
+const ANON_KEY = process.env.LOCAL_SUPABASE_ANON_KEY as string;
+// const ROLE_KEY = process.env.LOCAL_SUPABASE_ROLE_KEY as string
 
-// const SUPABASE_URL = process.env.REMOTE_SUPABASE_URL;
-// const ANON_KEY = process.env.REMOTE_SUPABASE_ANON_KEY;
+// const SUPABASE_URL = process.env.REMOTE_SUPABASE_URL as string
+// const ANON_KEY = process.env.REMOTE_SUPABASE_ANON_KEY as string
 
 export function createClient() {
-  return createServerClient<Database>(
-    // biome-ignore lint/style/noNonNullAssertion: <biome annoying>
-    SUPABASE_URL!,
-    // biome-ignore lint/style/noNonNullAssertion: <biome annoying>
-    ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return Object.entries(parseCookies()).map(([name, value]) => ({
-            name,
-            value,
-          }));
-        },
-        setAll(cookies) {
-          for (const cookie of cookies) {
-            setCookie(cookie.name, cookie.value);
-          }
-        },
+  return createServerClient<Database>(SUPABASE_URL, ANON_KEY, {
+    cookies: {
+      getAll() {
+        return Object.entries(parseCookies()).map(([name, value]) => ({
+          name,
+          value,
+        }));
+      },
+      setAll(cookies) {
+        for (const cookie of cookies) {
+          setCookie(cookie.name, cookie.value);
+        }
       },
     },
-  );
+  });
 }
